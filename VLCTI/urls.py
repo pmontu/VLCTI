e6032bb745ac3978d2acd031bb9a79941cf5d931
Manoj <pmontu@gmail.com>
2014-09-24 20:02:17 +0530

    home page routing; corrected l to L in controller cript adding

--- VLCTI/urls.py
+++ VLCTI/urls.py
@@ -3,10 +3,12 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 admin.autodiscover()
 
+
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'VLCTI.views.home', name='home'),
-     url(r'^institute/', include('institute.urls')),
+    url(r'^$', 'VLCTI.views.home'),
+
+    url(r'^institute/', include('institute.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
 )
